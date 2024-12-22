6b963ffdcc2c687fcce73db170c720da8b4e897a
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-12-22 14:55:44 +0100

    :art: type  :art:

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,10 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'no-unused-vars': 'warn',
+      'prefer-const': 'off',
+      '@typescript-eslint/no-explicit-any': 'off', 
+      '@typescript-eslint/no-unused-vars': ['warn'],
     },
   },
 )
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-
 import './App.css'
+
+let name:string = 'John Doe';
+let age:number = 25;
+let isMarried:boolean = false;
+let ages: number[] = [25, 30, 35];
+let person: any=3;
+person = 'John Doe';
 
 function App() {
 
