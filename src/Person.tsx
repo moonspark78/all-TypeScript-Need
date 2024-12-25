f5cd7d75d590454890c05b1ae3aca9277317adc8
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-12-25 14:21:55 +0100

    :art: input et state  :art:

--- src/Person.tsx
+++ src/Person.tsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
+
 interface PersonProps {
     name: string;
     age: number;
     IsMarried: boolean;
-  }
+}
 
-const Person = ({ name, age, IsMarried }:PersonProps) => {
-  return (
-    <div>
-        <p>Name: {name} </p>
-        <p>age: {age} </p>
-        <p>this person: {IsMarried ? "is Married" : "is single"} </p>
-    </div>
-  )
+const Person = (props: PersonProps) => {
+    const [isShowInfo, setIsShowInfo] = useState<boolean | null>(false)
+    const [bio, setBio] = useState<string | null>(null)
+    
+    const toggleInfo = () => {
+        setIsShowInfo(!isShowInfo)
+    }
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
+        setBio(event.target.value)
+    }
+
+    return (
+        <div>
+                <>
+                    <p>Name: {props.name} </p>
+                    <p>age: {props.age} </p>
+                    <p>this person: {props.IsMarried ? "is Married" : "is single"} </p>
+                </>
+        
+
+           {/*  <button onClick={toggleInfo}>Toggle Info</button> */}
+           <p>{props.name} Bio : {!bio ? "No person Availble" : bio} </p>
+           <input onChange={handleChange}  />
+        </div>
+    )
 }
 
 export default Person
